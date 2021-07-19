0662cf0ec2d5784d568d6e9bc8dd9c3128dc3409
liangali <liang.a.li@intel.com>
2021-07-19 21:23:55 +0800

    create d3d device

--- python/test.py
+++ python/test.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append(os.path.dirname(__file__)+'\\..\\build\\Debug')
 import pydxva as pyva
 
-print(pyva.add(1, 2))
+ret = pyva.create_device()
+print(ret)
 
 print('done')
--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,24 @@
 #include <pybind11/pybind11.h>
 
-int add(int i, int j) {
-    return i + j;
+#include <windows.h>
+#include <D3D11.h>
+#include <dxva.h>
+
+int pyCreateDevice()
+{
+    HRESULT hr = S_OK;
+    ID3D11Device* pD3D11Device = NULL;
+    ID3D11DeviceContext* pDeviceContext = NULL;
+    D3D_FEATURE_LEVEL levels[] = { D3D_FEATURE_LEVEL_11_1 };
+    D3D_FEATURE_LEVEL fl;
+
+    hr = D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, levels, 1,
+        D3D11_SDK_VERSION, &pD3D11Device, &fl, &pDeviceContext);
+ 
+    return hr;
 }
 
 PYBIND11_MODULE(pydxva, m) {
     m.doc() = "pybind11 example plugin"; 
-    m.def("add", &add, "A function which adds two numbers");
+    m.def("create_device", &pyCreateDevice, "Create Device");
 }
