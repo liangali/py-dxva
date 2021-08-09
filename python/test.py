b0144cdfc15dad27964409ca682b8871754673cb
liangali <liang.a.li@intel.com>
2021-08-09 10:53:22 +0800

    refine ctypes struct initialization

--- python/test.py
+++ python/test.py
@@ -10,21 +10,11 @@ w, h = 320, 240
 pyva.init()
 
 dec_cfg = D3D11VideoDecoderConfig()
-# print(dec_cfg)
 
-td = D3D11TextureDesc()
-td.Width, td.Height, td.MipLevels, td.ArraySize, td.Format = w, h, 1, 1, DXGI_FORMAT.NV12
-td.SampleDesc.Count, td.SampleDesc.Quality = 1, 0
-td.Usage = D3D11_USAGE.DEFAULT
-td.BindFlags = D3D11_BIND.DECODER
-td.CPUAccessFlags = 0
-td.MiscFlags = D3D11_RESOURCE_MISC.SHARED
+td = D3D11TextureDesc(w, h, 1, 1, DXGI_FORMAT.NV12, DXGISampleDesc(1, 0), D3D11_USAGE.DEFAULT, D3D11_BIND.DECODER, 0, D3D11_RESOURCE_MISC.SHARED)
 surf_rt = pyva.create_texture2d(np_from_struct(td))
 
-td.Usage = D3D11_USAGE.STAGING
-td.BindFlags = 0
-td.CPUAccessFlags = D3D11_CPU_ACCESS.READ
-td.MiscFlags = 0
+td = D3D11TextureDesc(w, h, 1, 1, DXGI_FORMAT.NV12, DXGISampleDesc(1, 0), D3D11_USAGE.STAGING, 0, D3D11_CPU_ACCESS.READ, 0)
 surf_staging = pyva.create_texture2d(np_from_struct(td))
 
 pl = pyva.profiles()
@@ -38,5 +28,6 @@ print('decoder = %x'%decoder)
 pyva.release_texture2d(surf_rt)
 pyva.release_texture2d(surf_staging)
 pyva.release_decoder(decoder)
+
 pyva.close()
 print('done')
